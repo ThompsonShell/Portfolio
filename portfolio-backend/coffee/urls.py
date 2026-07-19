b9c55aea7b6b97c615f5b4430080d7402a4f8ca7
AsilbekRajab <asilbek.rajabov.official@gmail.com>
2026-07-19 15:28:38 +0500

    refactor: replace DefaultRouter with SimpleRouter in URL configurations

--- portfolio-backend/coffee/urls.py
+++ portfolio-backend/coffee/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import SimpleRouter
 from .views import CoffeeRequestViewSet
 
-router = DefaultRouter()
+router = SimpleRouter()
 router.register("coffee-requests", CoffeeRequestViewSet, basename="coffee-request")
 
 urlpatterns = [
--- portfolio-backend/lectures/urls.py
+++ portfolio-backend/lectures/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import SimpleRouter
 from .views import LectureViewSet
 
-router = DefaultRouter()
+router = SimpleRouter()
 router.register("lectures", LectureViewSet, basename="lecture")
 
 urlpatterns = [
